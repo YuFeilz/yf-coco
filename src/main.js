c26f20799477cdf4aab5887061122265919c67b7
liucaifei <muziliu0124@163.com>
2018-05-28 15:29:03 +0800

    -

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决移动端点击300毫秒延迟问题
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 初始化css样式
 import './assets/styles/reset.css'
 // 移动端一像素边框问题
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
-// 解决移动端点击300毫秒延迟问题
-import fastClick from 'fastclick'
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
