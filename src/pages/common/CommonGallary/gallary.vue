aebb659de3d67c766048c1703367526262c0a9f6
liucaifei <muziliu0124@163.com>
2018-07-18 11:28:11 +0800

    -

---
+++ src/pages/common/CommonGallary/gallary.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex-box" @click="handleClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img :src="item" alt="">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        loop: true,
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .flex-box >>> .swiper-pagination
+    color #fff
+    bottom -1rem
+  .flex-box
+    display flex
+    flex-direction column
+    justify-content center
+    position fixed
+    top 0
+    left 0
+    right 0
+    bottom 0
+    z-index 99
+    background-color #000
+    .wrapper
+      background-color #fff
+      width 100%
+      .swiper-container
+        overflow inherit
+      img
+        width 100%
+</style>
--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="banner">
-    <img src="http://img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_600x330_5d562f69.jpg" alt="">
-    <div class="desc">
-      <div class="desc-iconfont"> <span class="iconfont">&#xe60d;</span> 15</div>
-      <p class="desc-text">
-        金海湖风景区（AAAA景区）
-      </p>
+  <div>
+    <div class="banner">
+      <img @click="handleClick" src="http://img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_600x330_5d562f69.jpg" alt="">
+      <div class="desc">
+        <div class="desc-iconfont"> <span class="iconfont">&#xe60d;</span> 15</div>
+        <p class="desc-text">
+          金海湖风景区（AAAA景区）
+        </p>
+      </div>
+      <router-link tag="span" to="/" class="banner-back iconfont" key="detbanner">&#xe656;</router-link>
     </div>
-    <router-link tag="span" to="/" class="banner-back iconfont" key="detbanner">&#xe656;</router-link>
+    <gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClick"></gallary>
   </div>
 </template>
 <script>
+import Gallary from '../../common/CommonGallary/gallary'
 export default {
-  name: 'Banner'
+  name: 'Banner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1806/39/393a31cf80de8e27a3.img.jpg_r_800x800_b50f8d20.jpg', 'http://img1.qunarzz.com/sight/p0/1806/e4/e4317bd6f21371e8a3.img.jpg_r_800x800_a290e308.jpg']
+    }
+  },
+  components: {
+    Gallary
+  },
+  methods: {
+    handleClick () {
+      this.showGallary = true
+    },
+    handleGallaryClick () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
